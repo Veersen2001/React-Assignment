8f6c3aefdc01440a8666b5507c4c14ed0a4a8d32
Veerbahadur Sen <veerbahadursen@gmail.com>
2023-08-14 13:22:35 +0530

    React Project

--- Gallery/src/components/ImageList/ImageList.jsx
+++ Gallery/src/components/ImageList/ImageList.jsx
@@ -2,37 +2,54 @@ import axios from 'axios'
 import { useEffect, useState } from 'react';
 import './ImagesList.css';
 import ImageShow from '../ImageShow/ImageShow';
+import {  useNavigate } from 'react-router-dom';
 function ImageList()
 {
+  //  images List state
     const [imageList, setImageList] = useState([]);
-
+    
+    // image Loading State
     const[isLoading, setIsLoading] = useState(true);
-   const ImageURL = 'https://api.slingacademy.com/v1/sample-data/photos?offset=0&limit=22'
+
+    const navigate = useNavigate();
+    // Images API url
+   const ImageURL = 'https://api.slingacademy.com/v1/sample-data/photos?offset=0&limit=30'
     async function downloadImages()
     {
-      const response = await axios.get(ImageURL);
+      try{
 
+        const response = await axios.get(ImageURL);
+        
         //  console.log(response.data);
-
+        
         const ImagesResult = response.data.photos;
+        
+        const res = ImagesResult.map((img)=>{
+          
+          return{
+            id:img.id,
+            image:img.url,
+            title:img.title
+            
+          }
+          
+        })
+        setImageList(res); 
+        setIsLoading(false);    
+      }
+      catch(err){
+         if(err)
+         {
+            navigate('*');
+           setIsLoading(false);
+         }
+      }
 
-       const res = ImagesResult.map((img)=>{
-      
-        return{
-           id:img.id,
-           image:img.url,
-           title:img.title
-           
-        }
-
-       })
-
-       setImageList(res);     
      
       // console.log(res);
       // console.log(imageList);
 
-       setIsLoading(false);
+       
     }
 
     useEffect(()=>{
@@ -44,7 +61,18 @@ function ImageList()
         
         <div className="images-list">
          
-          {(isLoading) ? <h1 className='image-loading'>Images Loading.....</h1> : imageList.map((p)=><ImageShow  title={p.title} image={p.image} key={p.id} id={p.id} />)}
+          {(isLoading) ? <div className="center">
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+          </div> : imageList.map((p)=><ImageShow  title={p.title} image={p.image} key={p.id} id={p.id} />)}
         </div>
         
       </div>
